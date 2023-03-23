37c1de9aae0fc33451af05b518826a51cb45497e
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2023-03-23 11:19:44 +0530

    tailwind added and basic frontend initialisation

--- frontend/src/pages/CallList.js
+++ frontend/src/pages/CallList.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function CallList() {
   return (
-    <div>CallList</div>
+    <>
+    <Header />
+    <div className='flex mt-2 ml-2'>
+        <div className='left-bar w-4/12'>
+            <p>Contacts</p>
+        </div>
+        <div className='right-bar w-7/12'>
+            <p>All Calls</p>
+            <div className='sorting-option flex gap-x-4'>
+                <p>Sort By</p>
+                <p>Date</p>
+                <p>Call Duration</p>
+            </div>
+        </div>
+    </div>
+    </>
+
   )
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Header from '../components/Header'
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Header />
+    <div>
+        <h2>This is HomePage</h2>
+    </div>
+    </>
   )
 }
 
