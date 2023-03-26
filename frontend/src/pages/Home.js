cb075cd416268a3165473df996d7ec6f836c49b3
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2023-03-26 14:15:16 +0530

    Home Page set

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,11 @@ function Home() {
   return (
     <>
     <Header />
-    <div>
-        <h2>This is HomePage</h2>
+    <div className='flex flex-col justify-center content-center items-center mt-5 gap-y-3'>
+        <p className='text-2xl' >HomePage</p>
+        <p>A tool that allows users to record and transcribe Zoom calls, and view their
+recordings and transcriptions on a call page.</p>
+        <img src='https://www.freepnglogos.com/uploads/zoom-logo-png/zoom-png-logo-download-transparent-20.png'  alt='hero'/>
     </div>
     </>
   )
